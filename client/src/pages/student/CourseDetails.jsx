32382d3cb0aa83d3a37cdb3700637ab94860d216
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-11-25 14:21:34 +0530

    Update CourseDetails.jsx

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -22,7 +22,7 @@ const CourseDetails = () => {
   return (
     <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
 
-      <div></div> 
+      <div className='absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70'></div> 
 
       {/* left colomn */}
       <div></div>
