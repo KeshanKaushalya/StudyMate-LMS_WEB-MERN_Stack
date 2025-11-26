72384f65cc50296c7894604f32973e79db52fc9f
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-11-27 01:15:52 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -12,6 +12,7 @@ export const AppContextProvider = (props) => {
 
     const [allCourses, setAllCourses] = useState([])
     const [isEducator, setIsEducator] = useState(true)
+    const [enrolledCourses, setEnrolledCourses] = useState([])
 
     // Fetch all courses 
     const fetchAllCourses = async () => {
@@ -59,13 +60,20 @@ export const AppContextProvider = (props) => {
         return totalLectures;
 
     }
+
+    // Fetch User Enrolled Courses
+    const fetchUserEnrolledCourses = async ()=>{
+        setEnrolledCourses(dummyCourses)
+    }
     
     useEffect(()=>{
         fetchAllCourses()
+        fetchUserEnrolledCourses()
     },[])
 
     const value = {
-        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures
+        currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, 
+        calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, enrolledCourses, fetchUserEnrolledCourses
 
     }
 
