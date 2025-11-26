9b2c47b5ba2b35928a7a9485b0d5dc8194dddc14
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-11-26 12:55:58 +0530

    Update CourseDetails.jsx

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -12,7 +12,7 @@ const CourseDetails = () => {
    const [courseData, setCoursedata] = useState(null)
    const [openSections, setOpenSection] = useState({})
 
-   const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures} = useContext(AppContext)
+   const {allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency} = useContext(AppContext)
 
    const fetchCourseData = async ()=>{
      const findCourse = allCourses.find(course => course._id === id)
@@ -65,7 +65,8 @@ const CourseDetails = () => {
                 <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
                   <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=> toggleSection(index)}>
                     <div className='flex items-center gap-2'>
-                      <img src={assets.down_arrow_icon} alt="arrow icon" />
+                      <img className={`transform transition-transform ${openSections[index] ? 'rotate-180' : ''}`}
+                       src={assets.down_arrow_icon} alt="arrow icon" />
                       <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
                     <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateChapterTime(chapter)}</p>
@@ -92,13 +93,58 @@ const CourseDetails = () => {
               ))}
            </div>
       </div>
+        
+        <div className='py-20 text-sm md:text-default'>
+          <h3 className='text-xl font-semibold text-gray-800'>Course Description</h3>
+           <p className='pt-3 rich-text'
+            dangerouslySetInnerHTML={{__html: courseData.courseDescription}}></p>
+        </div>
 
       </div>
 
   {/* right column (placeholder) */}
-  <div className='relative z-10' />
+  <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
+    <img src={courseData.courseThumbnail} alt="" />
+    <div className='p-5'>
+      <div className='flex items-center gap-2'>
+        <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+        <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price!</p>
+      </div>
+
+        <div className='flex gap-3 items-center pt-2'>
+          <p className='text-gray-800 md:text-4xl text-2xl font-semibold'>{currency}{(courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2)}</p>
+          <p className='md:text-lg text-gray-500 line-through'>{currency}{courseData.coursePrice}</p>
+          <p className='md:text-lg text-gray-500'>{courseData.discount}% off</p>
+        </div>
+
+        <div className='flex items-center text-sm md:text-default gap-4 pt-2 md:pt-4 text-gray-500'>
+
+          <div className='flex items-center gap-1'>
+            <img src={assets.star} alt="star icon" />
+            <p>{calculateRating(courseData)}</p>
+          </div>
+
+          <div className='h-4 w-px bg-gray-500/40'></div>
+
+          <div className='flex items-center gap-1'>
+            <img src={assets.time_clock_icon} alt="clock icon" />
+            <p>{calculateCourseDuration(courseData)}</p>
+          </div>
+
+          <div className='h-4 w-px bg-gray-500/40'></div>
+
+          <div className='flex items-center gap-1'>
+            <img src={assets.time_clock_icon} alt="clock icon" />
+            <p>{calculateNoOfLectures(courseData)} lessons</p>
+          </div>
+
+        </div>
 
     </div>
+  </div>
+              
+    </div>
+    
     </>
   ): <Loading />
 }
