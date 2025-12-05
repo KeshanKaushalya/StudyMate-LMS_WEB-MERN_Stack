5e9d9de791f4d40221e70460bfd242351bcf3238
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-05 21:57:18 +0530

    Update courseController.js

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -13,3 +13,24 @@ export const getAllCourses = async (req, res) => {
 }
 
 // Get Course by Id
+export const getCourseId = async (req, res)=>{
+    const {id} = req.params
+
+    try {
+        const courseData = await Course.findById(id).populate({path: 'educator'})
+
+        // Remove lectureUrl if isPreviewFree is false
+        courseData.courseContent.forEach(chapter => {
+            chapter.chapterContent.forEach(lecture =>{
+                if(!lecture.isPreviewFree){
+                    lecture.lectureUrl = "";
+                }
+            })
+        })
+
+        res.json({success: true, courseData})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
