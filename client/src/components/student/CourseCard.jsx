5914be845edc6fb9c21b30f83f6ff40a7ef0f4a2
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-09 12:01:31 +0530

    Update CourseCard.jsx

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -22,7 +22,12 @@ const CourseCard = ({course}) => {
           </div>
           <p className='text-gray-500'>{course.courseRatings.length}</p>
         </div>
-        <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
+        <p className='text-base font-semibold text-gray-800'>
+          {(course.coursePrice - course.discount * course.coursePrice / 100) <= 0 
+            ? 'Free' 
+            : `${currency}${(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}`
+          }
+        </p>
       </div>
     </Link>
   )
